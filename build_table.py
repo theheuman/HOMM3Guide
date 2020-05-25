33df2b5a248f5e794f24cfc919d5bc4428254e66
theheuman <jacob.heuman@gmail.com>
2020-05-24 23:25:05 -0400

    gitignore pycache, testing out my datatables library

---
+++ build_table.py
@@ -0,0 +1,68 @@
+# Until i get my library into pip we'll have to do some stupid python imports, to get to my sibling package
+# Directory structure looks like this:
+# development
+#     - HOMM3Guide
+#     - jinja_datatables
+import os
+import sys
+import importlib
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+
+import sys
+from pathlib import Path # if you haven't already done so
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError: # Already removed
+    pass
+
+from jinja_datatables.jinja_datatables.datatable_classes import DatatableColumn, JSArrayDatatable
+from jinja_datatables.jinja_datatables.jinja_extensions.datatableext import DatatableExt
+
+if __name__ == '__main__' and __package__ is None:
+    # include this column later
+    # className: "details-control", orderable: false, data: null, defaultContent: "", target: 0
+    columns = [
+        DatatableColumn("townName", "Town", "select"),
+        DatatableColumn("imageSrc", "Image", "select",
+                        {"render": "function ( data, type, row, meta) {return '<img src=\"' + data +'\"/>';}"}),
+        DatatableColumn("name", "Name", "text"),
+        DatatableColumn("level", "Level", "select"),
+        DatatableColumn("type", "Type", "select"),
+        DatatableColumn("damage", "Damage", "text"),
+        DatatableColumn("health", "Health", "text"),
+        DatatableColumn("speed", "Speed", "text"),
+        DatatableColumn("specials", "Specials", "text"),
+    ]
+    js_array = "troopInformation"
+    html_arguments = {"id": "example", "class": "display", "style": "width:100%"}
+    datatable_arguments = {
+        "fixedHeader": "true",
+        "order": '[[ 1, "asc" ]]',
+        # "columnDefs": '[{className: "dt-center ", targets: "_all"},{ "targets": [0], "searchable": false, "orderable": false, "visible": true }]'
+    }
+    table_view = JSArrayDatatable(
+        columns,
+        html_arguments,
+        datatable_arguments,
+        js_array,
+    )
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    template_dir = os.path.join(current_dir, "./")
+    env = Environment(loader=FileSystemLoader(template_dir), extensions=[DatatableExt])
+
+    template = env.get_template("jinja_index.html")
+    html = template.render(table_view=table_view)
+    print(html)
+    with open("index2.html", 'w') as fp:
+        fp.write(html)
+
+
+
